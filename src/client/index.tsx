b18c4c8c5e93b5c4f73ea6fd0b89233ac3afea8e
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-08-28 14:57:27 -0400

    dark mode implemented into index.tsx to include all of App routes

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -9,15 +9,42 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 
 const darkTheme = createTheme({
-     palette: {
-       mode: 'dark',
-       primary: {
-         main: 'rgba(255, 82, 82, 1)',
+       palette: {
+         mode: 'dark',
+         primary: {
+           main: '#008080', // Teal
+           dark: '#006666',
+           light: '#66B2B2',
+         },
+         secondary: {
+           main: '#ff4081', // Pink
+         },
+         background: {
+           default: '#303030',
+         },
+         text: {
+           primary: '#ffffff',
+           secondary: '#909090',
+         },
        },
-     },
-   });
-
-
+       typography: {
+         fontFamily: 'Roboto',
+         fontSize: 14,
+         fontWeightLight: 300,
+         fontWeightRegular: 400,
+         fontWeightBold: 700,
+       },
+       spacing: 2, // Default spacing value
+       breakpoints: {
+         values: {
+           xs: 0,
+           sm: 600,
+           md: 900,
+           lg: 1200,
+           xl: 1536,
+         },
+       },
+     });
 const container = document.getElementById('root')!;
 
 const root = createRoot(container);
