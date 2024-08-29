c2d54357b830f530b481307c14343f5985f4adda
Paloma Reynolds <palomap.paula@hotmail.com>
2024-08-28 20:51:51 -0400

    merge dev into paloma/graphql

--- build/src/client/App.d.ts
+++ build/src/client/App.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
 import './assets/styles.css';
+import * as React from 'react';
 declare const App: React.FC;
 export default App;
---
+++ build/src/client/footer.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare const Footer: React.FC;
+export default Footer;
