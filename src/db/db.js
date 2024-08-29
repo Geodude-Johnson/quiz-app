1f8abe19ff4b38efb825ac09cc34a140d71f1450
Paloma Reynolds <palomap.paula@hotmail.com>
2024-08-29 16:30:08 -0400

    connected graphql db

---
+++ src/db/db.js
@@ -0,0 +1,41 @@
+const { createHandler } = require("graphql-http/lib/use/express");
+const { buildSchema } = require("graphql");
+const express = require('express');
+const router = express();
+const mockData = require('../MOCK_DATA.json');
+var { ruruHTML } = require("ruru/server")
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    first_names: [String]
+    last_names: [String]
+
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const rootValue = {
+  first_names() {
+    return mockData.map(person => person.first_name + ' ' + person.last_name)
+  },
+  last_names() {
+    return mockData.map(person => person.last_name)
+  },
+};
+
+// Create and use the GraphQL handler.
+router.all(
+  "/graphql",
+  createHandler({
+    schema,
+    rootValue,
+  })
+);
+
+
+router.get("/db", (_req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+})
+module.exports = router;
