3ce6d6bbe853af873566db591f224fa8c604d4ab
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-08-31 13:12:03 -0400

    collections skeleton styled, awaiting dynamic content

--- src/client/home/components/collections.tsx
+++ src/client/home/components/collections.tsx
@@ -1,3 +1,10 @@
+/*skeleton for collections is set up, need to add dynamic rendering so that the collections will 
+onyl appear if the collection exists 
+*dynamically render the collection title and description 
+based off user input
+
+*/
+
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -5,38 +12,83 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import { styled } from '@mui/material/styles';
+import collectionLogo from '../../assets/collectionLogo.png';
 
+const StyledCollection1 = styled('div')(({ theme }) => ({
+  display: 'grid',
+  gridTemplateRows: '1fr 1fr 1fr',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  position: 'relative',
+  top: 70,
+  left: 360,
+  width: '50%',
+  gap: '8px',
+}));
 
-const StyledCollection = styled('div')(({ theme }) => ({
-    position: 'relative',
-    top: 70,
-    left: 359,
-  }));
-
-function CardCollections () {
+function CardCollections() {
   return (
-    <StyledCollection>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/client/assets/collectionLogo.png"
-          alt="collection-logo"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" >
-            User collection 1 name
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'primary.light' }}>
-            User description of collection
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </StyledCollection>
+    <div>
+      <StyledCollection1>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={collectionLogo}
+              alt="collection-logo"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                User collection 1 name
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'primary.light' }}>
+                User description of collection
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+   
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={collectionLogo}
+              alt="collection-logo"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                User collection 2 name
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'primary.light' }}>
+                User description of collection
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={collectionLogo}
+              alt="collection-logo"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                User collection 2 name
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'primary.light' }}>
+                User description of collection
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        </StyledCollection1>
+    </div>
   );
 }
-
 
 export default CardCollections;
