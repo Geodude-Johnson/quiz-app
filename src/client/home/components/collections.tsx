56913e0d59c45027de73eed4bad0046b4960807d
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-08-31 14:33:48 -0400

    front end major skeleton changes and formatting, minor bug issues identified via components for the not fully built out search bar and add button.

--- src/client/home/components/collections.tsx
+++ src/client/home/components/collections.tsx
@@ -3,6 +3,7 @@ onyl appear if the collection exists
 *dynamically render the collection title and description 
 based off user input
 
+*addCard button not working
 */
 
 import * as React from 'react';
@@ -13,7 +14,10 @@ import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import { styled } from '@mui/material/styles';
 import collectionLogo from '../../assets/collectionLogo.png';
-
+import AddIcon from '@mui/icons-material/Add';
+import { Add } from '@mui/icons-material';
+import Fab from '@mui/material/Fab';
+/**collections styling  */
 const StyledCollection1 = styled('div')(({ theme }) => ({
   display: 'grid',
   gridTemplateRows: '1fr 1fr 1fr',
@@ -24,10 +28,37 @@ const StyledCollection1 = styled('div')(({ theme }) => ({
   width: '50%',
   gap: '8px',
 }));
+/**Add icon styling */
+const StyledFab = styled(Fab)({
+  position: 'relative',
+  zIndex: 1,
+  top: 3,
+  left: 285,
+  right: 0,
+  margin: '0 auto',
+});
+/**Add a card functionality */
+const handleAddCard = (e: any) => {
+  e.preventDefault();
+  return ('Clicked!');
+}
 
+/**Add a card button */
+function AddCard () {
+return (
+  <div>
+    <AddIcon onClick={handleAddCard}/>
+  </div>
+)
+}
+
+/**Entire User Collections */
 function CardCollections() {
   return (
     <div>
+      <StyledFab color="secondary" aria-label="add" className="styled-fab">
+      <AddCard />
+      </StyledFab>
       <StyledCollection1>
         <Card sx={{ maxWidth: 345 }}>
           <CardActionArea>
@@ -39,7 +70,7 @@ function CardCollections() {
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                User collection 1 name
+              User collection 1 name
               </Typography>
               <Typography variant="body2" sx={{ color: 'primary.light' }}>
                 User description of collection
