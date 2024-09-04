754b4a27c27b5ec038a43d3321bdc0733643112c
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-09-04 16:37:50 -0400

    search comment modified

--- src/client/home/components/search.tsx
+++ src/client/home/components/search.tsx
@@ -1,7 +1,7 @@
 /*
 * need to add dymanic functionality so we can search through the collections, function skeleton
-set up to console log 'searched when you press enter' */
-
+set up to console log 'searched' 
+this is working when the mouse leaves the block , not any other times */
 import * as React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { alpha, styled } from '@mui/material/styles';
